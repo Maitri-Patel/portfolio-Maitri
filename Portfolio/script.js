7d4df71f408624817126962be5baac86ff8199dd
Maitri-Patel <140112563+Maitri-Patel@users.noreply.github.com>
2024-01-15 01:02:21 -0600

    Delete Portfolio directory

--- Portfolio/script.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".text", {
-    strings: ["Full Stack Web Developer", "Aspiring Pilot 👩🏼‍✈️✈️", "Junior Web Developer", "Continuous Learner",],
-    typespeed: 20,
-    backspeed: 20,
-    backDelay: 1000,
-    loop: true
-})
